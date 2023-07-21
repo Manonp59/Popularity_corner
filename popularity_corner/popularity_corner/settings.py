b68aecaa9b02da11b67aed2c9ad2d13c71b39143
Manonp59 <manon-platteau@orange.fr>
2023-07-21 11:33:16 +0200

    scraping imdb

--- popularity_corner/popularity_corner/settings.py
+++ popularity_corner/popularity_corner/settings.py
@@ -17,15 +17,15 @@ NEWSPIDER_MODULE = "popularity_corner.spiders"
 #USER_AGENT = "popularity_corner (+http://www.yourdomain.com)"
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
-# See also autothrottle settings and docs
-DOWNLOAD_DELAY = 3
+# # See also autothrottle settings and docs
+DOWNLOAD_DELAY = 0.5
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
@@ -50,9 +50,11 @@ DOWNLOAD_DELAY = 3
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    "popularity_corner.middlewares.PopularityCornerDownloaderMiddleware": 543,
-#}
+DOWNLOADER_MIDDLEWARES = {
+   #"popularity_corner.middlewares.PopularityCornerDownloaderMiddleware": 543,
+   "popularity_corner.middlewares.ScrapOpsFakeUserAgentMiddleware":400,
+   
+}
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
@@ -91,3 +93,14 @@ DOWNLOAD_DELAY = 3
 REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
 TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
 FEED_EXPORT_ENCODING = "utf-8"
+
+SCRAPEOPS_API_KEY='42c723d6-9178-42a1-9eaa-5d6ecb8168ec'
+
+SCRAPEOPS_FAKE_USER_AGENT_ENDPOINT='https://headers.scrapeops.io/v1/user-agents'
+
+SCRAPEOPS_FAKE_USER_AGENT_ENABLED = True
+
+SCRAPEOPS_NUM_RESULTS= 50
+
+
+
