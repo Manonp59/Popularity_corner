0acb07b26f34957b4c2767090e4b324d6b4b786a
Ne'evaiti <52328785+neevaiti@users.noreply.github.com>
2023-07-25 20:56:00 +0200

    feat: adding tabs events

--- app/main/static/javascript/navbar.js
+++ app/main/static/javascript/navbar.js
@@ -5,3 +5,14 @@ $(document).ready(function(){
         $(this).addClass('active-tabs');
     });
 });
+
+// Tabs events
+$(document).ready(function(){
+    $('.tabs').click(function(){
+        $('.tabs').removeClass('active-tabs');
+        $(this).addClass('active-tabs');
+        $('.tab-content').hide();
+        var tab = $(this).attr('data-tab');
+        $('#' + tab).show();
+    });
+});
