4577063ec06fac8d34b20b754319c5866bde2e20
MaximeR12 <rowell.maxime2001@gmail.com>
2023-08-08 10:46:23 +0200

    🚀Final push Airflow scrapping automation

--- Popularity_corner/main/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class Upcoming_movie(models.Model):
-    title = models.fields.CharField(max_length =50)
-    release_date = models.fields.DateField(auto_now=False, auto_now_add=False)
-    estimated_audience = models.fields.IntegerField()
